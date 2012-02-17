7e2c1b5f78bc2428f63928a524a3253301917764
ipa-mig <matthias.gruhler@ipa.fraunhofer.de>
2012-02-17 16:42:06 +0100

    mig/323
    read parameter from file
    
    implement costmap subscriber
    
    check that transform is available

--- cob_joystick_filter/ros/src/cob_joystick_filter.cpp
+++ cob_joystick_filter/ros/src/cob_joystick_filter.cpp
@@ -71,6 +71,9 @@
 #include <geometry_msgs/Polygon.h>
 #include <nav_msgs/GridCells.h>
 
+// Listener includes
+#include <tf/transform_listener.h>
+
 //###############################
 //#### joystick filter class ####
 class JoystickFilterClass
@@ -91,7 +94,8 @@ class JoystickFilterClass
     m_mutex = PTHREAD_MUTEX_INITIALIZER;
 
     // node handle to get footprint from parameter server
-    ros::NodeHandle local_costmap_nh("/local_costmap_node/costmap"); 	
+    std::string costmap_name_ = "/local_costmap_node/costmap";
+    ros::NodeHandle local_costmap_nh_(costmap_name_); 	
 
 // implementation of topics to publish (command for base and list of relevant obstacles)
 //  	topic_pub_command_ = nh_.advertise<geometry_msgs::Twist>("command", 1);
@@ -102,10 +106,46 @@ class JoystickFilterClass
   	// subscribe to the costmap to receive inflated cells
   	obstacles_sub_ = nh_.subscribe<nav_msgs::GridCells>("obstacles", 1, boost::bind(&JoystickFilterClass::obstaclesCB, this, _1));
 		
+		// read parameters from parameter server
+		if(!local_costmap_nh_.hasParam(costmap_name_+"/global_frame")) ROS_WARN("Used default parameter for global_frame");
+		local_costmap_nh_.param(costmap_name_+"/global_frame", global_frame_, std::string("/map"));
+		
+		if(!local_costmap_nh_.hasParam(costmap_name_+"/robot_base_frame")) ROS_WARN("Used default parameter for robot_frame");
+		local_costmap_nh_.param(costmap_name_+"/robot_base_frame", robot_frame_, std::string("/base_link"));
+		
+    if(!nh_.hasParam(name+"/influence_radius")) ROS_WARN("Used default parameter for influence_radius");
+		nh_.param(name+"/influence_radius", influence_radius_, 1.5);
+		closest_obstacle_dist_ = influence_radius_;
+		
+		if(!nh_.hasParam(name+"/stop_threshold")) ROS_WARN("Used default parameter for stop_threshold");
+		nh_.param(name+"/stop_threshold", stop_threshold_, 0.10);
+		
+		if(!nh_.hasParam(name+"/obstacle_damping_dist")) ROS_WARN("Used default parameter for obstacle_damping_dist");
+		nh_.param(name+"/obstacle_damping_dist", obstacle_damping_dist_, 50.0);
+		if(obstacle_damping_dist_ < stop_threshold_) {
+			ROS_WARN("obstacle_damping_dist < stop_threshold => robot will stop without decceleration!");
+		}
+		
+		if(!nh_.hasParam(name+"/use_circumscribed_threshold")) ROS_WARN("Used default parameter for use_circumscribed_threshold_");
+		nh_.param(name+"/use_circumscribed_threshold", use_circumscribed_threshold_, 0.20);
+		
     //load the robot footprint from the parameter server if its available in the local costmap namespace
-		robot_footprint_ = loadRobotFootprint(local_costmap_nh);
+		robot_footprint_ = loadRobotFootprint(local_costmap_nh_);
 		if(robot_footprint_.size() > 4) 
 			ROS_WARN("You have set more than 4 points as robot_footprint, cob_joystick_filter can deal only with rectangular footprints so far!");
+
+
+		//we need to make sure that the transform between the robot base frame and the global frame is available
+		ros::Time last_error = ros::Time::now();
+		std::string tf_error;
+		while(!tf_listener_.waitForTransform(global_frame_, robot_frame_, ros::Time(), ros::Duration(0.1), ros::Duration(0.01), &tf_error)) {
+			ros::spinOnce();
+			if(last_error + ros::Duration(5.0) < ros::Time::now()){
+				ROS_WARN("Waiting on transform from %s to %s to become available before running cob_joystick_filter, tf error: %s", 
+				robot_frame_.c_str(), global_frame_.c_str(), tf_error.c_str());
+				last_error = ros::Time::now();
+			}
+		}
   } 
   // joystick_velocityCB reads twist command from joystick
   void joystickVelocityCB(const geometry_msgs::Twist::ConstPtr &twist){
@@ -114,7 +154,15 @@ class JoystickFilterClass
   
   // obstaclesCB reads obstacles from costmap
   void obstaclesCB(const nav_msgs::GridCells::ConstPtr &obstacles){
-  	ROS_INFO("obstaclesCB called");
+		pthread_mutex_lock(&m_mutex);
+	
+		if(obstacles->cells.size()!=0) costmap_received_ = true;
+		last_costmap_received_ = * obstacles;
+		
+		if(stop_threshold_ < obstacles->cell_width / 2.0f || stop_threshold_ < obstacles->cell_height / 2.0f)
+			ROS_WARN("You specified a stop_threshold that is smaller than resolution of received costmap!");
+		
+		pthread_mutex_unlock(&m_mutex);
   }
   
   // Destructor
@@ -124,9 +172,17 @@ class JoystickFilterClass
   private:
   pthread_mutex_t m_mutex;
 
+  //tf transform
+	tf::TransformListener tf_listener_;
+	std::string global_frame_, robot_frame_;
+
   //obstacle avoidence
   std::vector<geometry_msgs::Point> robot_footprint_;
   double footprint_left_, footprint_right_, footprint_front_, footprint_rear_;
+  bool costmap_received_;
+  nav_msgs::GridCells last_costmap_received_;
+  double influence_radius_, stop_threshold_, obstacle_damping_dist_, use_circumscribed_threshold_;
+  double closest_obstacle_dist_;
 
   //helper functions:
   std::vector<geometry_msgs::Point> loadRobotFootprint(ros::NodeHandle node);
@@ -192,7 +248,7 @@ std::vector<geometry_msgs::Point> JoystickFilterClass::loadRobotFootprint(ros::N
 		if(footprint[i].y > footprint_left_) footprint_left_ = footprint[i].y;
 		if(footprint[i].y < footprint_right_) footprint_right_ = footprint[i].y;
 	}
-	ROS_INFO("Extracted rectangular footprint for cob_joystick_filter: Front: %f, Rear %f, Left: %f, Right %f", footprint_front_, footprint_rear_, footprint_left_, footprint_right_);
+	ROS_DEBUG("Extracted rectangular footprint for cob_joystick_filter: Front: %f, Rear %f, Left: %f, Right %f", footprint_front_, footprint_rear_, footprint_left_, footprint_right_);
 
 	return footprint;
 }
